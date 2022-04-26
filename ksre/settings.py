1b62d2a7a36af6274bf888f9cc1fe8ce49ae7324
EC2 Default User <ec2-user@ip-172-31-40-15.eu-west-2.compute.internal>
2022-04-26 00:32:06 +0000

    security key

--- ksre/settings.py
+++ ksre/settings.py
@@ -9,6 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'tq2er8qxd7szzqd*nm1z!w4#46_7fi7vcmyz%0kqo6ad(h^unv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
